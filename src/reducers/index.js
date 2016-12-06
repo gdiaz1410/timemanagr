8064c0a5d705cfe79bc6e5b771d33f04a78bd833
Gabriel Diaz <gdiaz1410@gmail.com>
2016-12-05 18:00:12 -0600

    login form and info

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
 import {combineReducers} from 'redux';
+import AuthReducer from './AuthReducer';
 
+// receives action
+// catch email change action and record it
+// content is property of state being produced
 export default combineReducers({
-  random: () => []
+  auth: AuthReducer
 })
